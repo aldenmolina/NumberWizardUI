6fea33e70534d596ccb33fe39ecda22e6f577413
Alden <aj.molina092@gmail.com>
2019-01-23 17:23:29 -0500

    Finished working game. Functional buttons.

---
+++ Assets/NumberWizard.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameStart();
+    }
+
+    void GameStart()
+    {       
+        guess = (max + min) / 2;
+        guessText.text = guess.ToString();
+        max++;
+    }
+
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+    // Update is called once per frame
+    
+    void NextGuess()
+    {
+        guess = (max + min) / 2;
+        guessText.text = guess.ToString();
+    }
+}
